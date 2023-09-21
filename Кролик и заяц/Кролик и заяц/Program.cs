2b1b1d77bfc284ddda9f49fe7a920cdeef996847
Сом <117897751+ILYA222213@users.noreply.github.com>
2023-09-21 08:20:56 +0300

    Add files via upload

---
+++ Кролик и заяц/Кролик и заяц/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void MyThreadTurtle()
+    {
+        for (int i = 0; i < 100; i++) //Цикл,который будет выполнятся 100 раз
+        {
+            Console.WriteLine("Черепаха прошла: " + i); //Выводим сообщение о прохождении черепахи с текущим значением i
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Thread turtle = new(MyThreadTurtle); //Создаем новый поток с методом MyThreadTurtle
+        turtle.Start(); //Запускаем поток
+
+        for (int i = 0; i < 100; i++) //Цикл, который будет выполняться 100 раз
+        {
+            Console.WriteLine("Кролик прошел: " + i); //Выводим сообщение о прохождении кролика с текущим значением i
+        }
+    }
+}
